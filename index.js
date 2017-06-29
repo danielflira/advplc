e425fb2f89633dca01095bfe912e6efc5e0e0a1c
Daniel Lira <danielflira@gmail.com>
2017-06-28 23:52:57 -0300

    adicionados recursos do wizard

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 var fs = require('fs');
 var path = require('path');
+var program = require('commander')
 var child_process = require('child_process');
+var prompt_wizard = require('prompt-wizard');
 
 var advplc = {
 
@@ -79,7 +81,12 @@ var advplc = {
 
     'loadConfigure': function(callback) {
         fs.readFile('.advplc', function(err, data) {
-            configure = JSON.parse(data);
+            var configure = {}
+            
+            if ( data ) {
+                configure = JSON.parse(data);
+            }
+
             if ( callback ) {
                 callback(configure);
             }
@@ -91,11 +98,204 @@ var advplc = {
     }
 };
 
-advplc.loadConfigure(function(configure) {
-    advplc.compileFile(path.resolve('asdf1.prw'), function(result) {
-        for ( let i = 0; i < result.msgs.length; i++ ) {
-            let msg = result.msgs[i];
-            console.log(msg);
+
+function compileResource(env, resource) {
+    advplc.loadConfigure(function(configure) {
+        advplc.compileFile(path.resolve(resource), function(result) {
+            for ( let i = 0; i < result.msgs.length; i++ ) {
+                let msg = result.msgs[i];
+                console.log(msg);
+            }
+        });
+    });
+}
+
+
+function runWizard(parameters) {
+    advplc.loadConfigure(function(configure) {
+
+        if ( parameters.cfg ) {
+            var wizard = prompt_wizard.create([
+                {
+                    'prompt': 'authorization generation date',
+                    'key': 'authorization_generation',
+                    'default': configure.authorization_generation
+                },
+                {
+                    'prompt': 'authorization validation date',
+                    'key': 'authorization_validation',
+                    'default': configure.authorization_validation
+                },
+                {
+                    'prompt': 'authorization permission',
+                    'key': 'authorization_permission',
+                    'default': configure.authorization_permission
+                },
+                {
+                    'prompt': 'authorization code',
+                    'key': 'authorization_code',
+                    'default': configure.authorization_code
+                },
+                {
+                    'prompt': 'start program from smartclient',
+                    'key': 'startProgram',
+                    'default': configure.startProgram ? 
+                        configure.startProgram :
+                        "sigaadv"
+                },
+                {
+                    'prompt': 'compile files regex',
+                    'key': 'compileFolderRegex',
+                    'default': configure.compileFolderRegex ? 
+                        configure.compileFolderRegex : 
+                        '.*\\.(prw|prx|apw|aph|tres|png|bmp|tres)'
+                }
+            ]);
+
+            wizard.execute().then(function(quiz){
+                Object.assign(configure, quiz);
+            });
+        }
+
+        if ( parameters.add ) {
+            let environment = {}
+
+            var wizard1 = prompt_wizard.create([
+                {
+                    'prompt': 'environment name',
+                    'key': 'environment',
+                    'default': 'environment'
+                }
+            ]);
+
+            wizard1.execute().then(function(quiz){
+                let update = false;
+
+                // first environment register
+                if ( ! configure.environments ) {
+                    configure.environments = [];
+                }
+
+                // search existent environment and load configuration
+                for ( let i = 0; i < configure.environments.length; i++ ) {
+                    let envcfg = configure.environments[i];
+                    if ( envcfg.environment.toLowerCase() == quiz.environment ) {
+                        environment = envcfg;
+                        update = true;
+                        break;
+                    }
+                }
+
+                // if environment doesn't exists create new
+                if ( ! environment.environment ) {
+                    Object.assign(environment, quiz);
+                }
+
+                var wizard2 = prompt_wizard.create([
+                    {
+                        'prompt': 'server binary version',
+                        'key': 'serverVersion',
+                        'default': environment.serverVersion ? 
+                            environment.serverVersion : 
+                            '131227A'
+                    },
+                    {
+                        'prompt': 'server address',
+                        'key': 'server',
+                        'default': environment.server ? 
+                            environment.server : 
+                            'localhost'
+                    },
+                    {
+                        'prompt': 'server port',
+                        'key': 'port',
+                        'default': environment.port ? 
+                            environment.port : 
+                            '5555'
+                    },
+                    {
+                        'prompt': 'environment language',
+                        'key': 'language',
+                        'default': environment.language ? 
+                            environment.language : 
+                            'PORTUGUESE'
+                    },
+                    {
+                        'prompt': 'object repository type',
+                        'key': 'rpoType',
+                        'default': environment.rpoType ? 
+                            environment.rpoType : 
+                            'TOP'
+                    },
+                    {
+                        'prompt': 'environment user',
+                        'key': 'user',
+                        'default': environment.user ? 
+                            environment.user : 
+                            'admin'
+                    },
+                    {
+                        'prompt': 'environment password',
+                        'key': 'passwordCipher',
+                        'default': environment.passwordCipher ? 
+                            environment.passwordCipher : 
+                            ''
+                    },
+                    {
+                        'prompt': 'environment default start program',
+                        'key': 'startProgram',
+                        'default': environment.startProgram ? 
+                            environment.startProgram : 
+                            'sigaadv'
+                    },
+                    {
+                        'prompt': 'smartclient path',
+                        'key': 'smartClientPath',
+                        'default': environment.smartClientPath ? 
+                            environment.smartClientPath : 
+                            'C:\\TOTVS\\Protheus\\bin\\smartclient\\'
+                    },
+                    {
+                        'prompt': 'include list',
+                        'key': 'includeList',
+                        'default': environment.includeList ? 
+                            environment.includeList : 
+                            'C:\\caminho1\\include\\;C:\\caminho2\\include\\'
+                    },
+                ]);
+
+                wizard2.execute().then(function(quiz){
+                    advplc.cipherPassword(quiz.passwordCipher, function(cipher){
+                        quiz.passwordCipher = cipher;
+                        if ( ! update ) {
+                            configure.environments.push(quiz);
+                        }
+                    });
+                });
+            });
         }
     });
-});
+}
+
+
+program
+    .arguments('<resource>')
+    .option('-e, --env <env>', 'environment from .advplc to compile')
+    .option('--cfg', 'configure .advplc generic options')
+    .option('--add', 'add .advplc environment options')
+    .action(function(resource) {
+
+        // check if compile or if configure
+        if ( program.cfg || program.add ) {
+            runWizard(program);
+
+        // compile
+        } else {
+            if ( program.env ) {
+                compileResource(program.env, resource);
+            } else {
+                console.log('point .advplc environment to use')
+            }
+        }
+    })
+    .parse(process.argv)
